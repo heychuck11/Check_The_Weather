04787798d41ffeb9cc9a6b5c5f22a556462595c8
Charlie Howard <chuckhoward99@gmail.com>
2020-10-08 09:50:46 -0600

    spell check

--- script.js
+++ script.js
@@ -1,76 +1,70 @@
 var apiKey = "0f29c2c8847681ce41550d39bd7e97ef"
 
-var queryURL =   "https://api.openweathermap.org/data/2.5/weather?q=" + "city" + "&appid=" + apiKey;
 
-
-
-
-//var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
 
 $(document).ready(function(){
+  $("#searchBtn").on("click", function(){
+    var cityName = $("#searchInput").val()
+    $("#searchInput").val("")
+    searchWeather(cityName)
 
-  //show the weather in Rochester,NY
-  function showWeather(){
-    var cityDataOne = $(".cityOne").val();
-    var queryURLOne =  "https://api.openweathermap.org/data/2.5/weather?q=" + "Rochester,Ny" + "&appid=" + apiKey
+  })
 
+  function searchWeather(cityName){
     $.ajax({
-      url: queryURLOne,
-      method: "GET"
-    }).then(function(response){
+      type: "GET",
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey,
+      dataType: "json",
+      success: function(data){
+        console.log(data)
+        $("#currentWeather").empty()
+        var name = $("<h3>").text(data.name)
+        //var date 
+        var wind = $("<p>").addClass("card-text").text("wind speed" + data.wind.speed)
+        var humid = $("<p>").addClass("card-tex").text("humidity" + data.main.humidity) 
+        var temp = $("<p>").addClass("card-text").text("temp" + data.main.temp)
+        //var icon
+        var card = $("<div>").addClass("card")
+        var cardBody = $("<div>").addClass("card-body")
+        cardBody.append(name,wind,humid,temp)
+        card.append(cardBody)
+        $("#currentWeather").append(card)
+        getForecast(cityName)
 
-    
+      }
+     
     })
-    var cityDataTwo = $(".cityTwo").val();
-    var queryURLTwo=  "https://api.openweathermap.org/data/2.5/weather?q=" + "St. Paul, MN" + "&appid=" + apiKey
 
-    $.ajax({
-      url: queryURLTwo,
-      method: "GET"
-    }).then(function(response){
-
-    
-    })
-    var cityDataThree = $(".cityThree").val();
-    var queryURLThree =  "https://api.openweathermap.org/data/2.5/weather?q=" + "New York City, NY" + "&appid=" + apiKey
-
-    $.ajax({
-      url: queryURLThree,
-      method: "GET"
-    }).then(function(response){
-
-    
-    })
-    var cityDataFour = $(".cityFour").val();
-    var queryURLFour =  "https://api.openweathermap.org/data/2.5/weather?q=" + "San Diego, CA" + "&appid=" + apiKey
-
-    $.ajax({
-      url: queryURLFour,
-      method: "GET"
-    }).then(function(response){
-
-    
-    })
-    var cityDataFive = $(".cityFive").val();
-    var queryURLFive =  "https://api.openweathermap.org/data/2.5/weather?q=" + "Miami, FL" + "&appid=" + apiKey
-
-    $.ajax({
-      url: queryURLFive,
-      method: "GET"
-    }).then(function(response){
-
-    
-    })
-    var cityDataSix= $(".citySix").val();
-    var queryURLSix =  "https://api.openweathermap.org/data/2.5/weather?q=" + "Buffalo, Ny" + "&appid=" + apiKey
-
-    $.ajax({
-      url: queryURLSix,
-      method: "GET"
-    }).then(function(response){
-  
-    
-    })
   }
 
-  
+function getForecast(cityName){
+  $.ajax({
+    type: "GET",
+      url: "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey,
+      dataType: "json",
+      success: function(data){
+        console.log(data)
+        for(i=0; i < data.list.length; i++){
+        if(data.list[i].dt_txt.indexOf("15:00:00") !==1) {
+          var col = $("<div>").addClass("col-md-2")
+          var card = $("<div>").addClass("card bg-secondary text-white")
+          var cardBody = $("<div>").addClass("card-body p-2")
+          var name = $("<h5>").addClass("card-title").text(data.city.name)
+          //var date
+          //var icon
+          var temp = $("<p>").addClass("card-text").text("t: " + data.list[i].main.temp)
+          var humid = $("<p>").addClass("card-text").text("h: " + data.list[i].main.humidity)
+          col.append(card.append(cardBody.append(name,temp,humid)))
+          $("#forecastWeather").append(col)
+
+
+        }
+      }
+      
+      
+      }
+  })
+}
+
+
+})
